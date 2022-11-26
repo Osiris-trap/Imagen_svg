1b2ae9bbd1b8a7273d06be02f04b6bcf71a18490
Osiris-trap <melendezernesto777@gmail.com>
2022-11-26 00:38:30 -0600

    SVG

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SVG</title>
+    <style>
+        body{
+
+            background-color: pink;
+        }
+        .circulo{
+            width: 40%;
+            magin: auto;
+        
+        }
+        .circulo{
+            stroke: blue;
+            stroke-width: 2px;
+
+        }
+        .circulo :hover{
+            fill: gray;
+            cursor: pointer;
+            stroke : yellow
+
+        }
+    </style>
+</head>
+<body>
+    
